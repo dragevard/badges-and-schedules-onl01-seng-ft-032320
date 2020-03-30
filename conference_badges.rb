d25ed8f31acb9ed3808dc3fbe5485805563ac54c
Derek phung <dragevards@gmail.com>
2020-03-30 21:52:57 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,26 @@
-# Write your code here.
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(array)
+  messages = []
+  array.each do |message|
+    messages.push("Hello, my name is #{message}.")
+  end
+  messages
+end
+
+def assign_rooms(array)
+  room = 1
+  message = []
+  array.each do |speaker|
+    message.push("Hello, #{speaker}! You'll be assigned to room #{room}!")
+    room += 1
+  end
+  message
+end
+
+def printer(result)
+  
+  puts batch_badge_creator(result)
+end

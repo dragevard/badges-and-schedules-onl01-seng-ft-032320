1d7ac9266ace88f6d8d5221d1b1c8fcb9c516d42
Derek phung <dragevards@gmail.com>
2020-03-30 21:59:25 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,30 @@
-# Write your code here.
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(array)
+  messages = []
+  array.each do |message|
+    messages.push("Hello, my name is #{message}.")
+  end
+  messages
+end
+
+def assign_rooms(array)
+  room = 1
+  message = []
+  array.each do |speaker|
+    message.push("Hello, #{speaker}! You'll be assigned to room #{room}!")
+    room += 1
+  end
+  message
+end
+
+def printer(array)
+  batch_badge_creator(array).each do |item|
+    puts item
+  end
+  assign_rooms(array).each do |item|
+    puts item
+  end
+end
